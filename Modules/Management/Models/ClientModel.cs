06a091e1110b71e98800a01414b118754c7b263b
Luega <martinelliluca90@gmail.com>
2023-07-20 11:27:11 +0200

    added ClientModel and Request_ClientModel

---
+++ Modules/Management/Models/ClientModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace therapyFlow.Modules.Management.Models
+{
+    public class ClientModel
+    {
+        public int Id { set; get; }
+        public string FirstName { set; get; } = "";
+        public string LastName { set; get; } = "";
+    }
+}
---
+++ Modules/Management/Models/Request_ClientModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace therapyFlow.Modules.Management.Models
+{
+    public class Request_ClientModel
+    {
+        public string FirstName { set; get; } = "";
+        public string LastName { set; get; } = "";
+    }
+}
